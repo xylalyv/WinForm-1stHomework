10a3d0ffb8e4514d2dc34aa69b7ab08ad9045d9d
xeyal.ramile15@gmail.com <xeyal.ramile15@gmail.com>
2023-05-28 01:37:14 +0400

    Last commit

---
+++ HomeWork1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace HomeWork1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(591, 92);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Baku";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(591, 136);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 1;
+            button2.Text = "London";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(225, 164);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            label1.MouseHover += label1_MouseHover;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+
+            Paint += MainForm_Paint;
+            MouseDoubleClick += Form1_MouseDoubleClick;
+            MouseDown += MainForm_MouseDown;
+            MouseMove += MainForm_MouseMove;
+            MouseUp += MainForm_MouseUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+    }
+}
---
+++ HomeWork1/Form1.cs
@@ -0,0 +1,130 @@
+﻿using HomeWork1.Properties;
+
+namespace HomeWork1
+{
+    public partial class Form1 : Form
+    {
+
+        private bool checkDraw;
+        private Point startPoint;
+        private Point endPoint;
+        private int rectangleCount;
+        private List<Rectangle> rectangles;
+        public Form1()
+        {
+            InitializeComponent();
+            checkDraw = false;
+            rectangles = new List<Rectangle>();
+            rectangleCount = 0;
+        }
+        //Task 1
+        private void MainForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                checkDraw = true;
+                startPoint = e.Location;
+                endPoint = e.Location;
+            }
+        }
+
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (checkDraw)
+            {
+                endPoint = e.Location;
+                Refresh();
+            }
+        }
+
+        private void MainForm_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                checkDraw = false;
+                Rectangle rectangle = new Rectangle(startPoint.X, startPoint.Y, endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+                if (rectangle.Width < 10 || rectangle.Height < 10)
+                {
+                    checkDraw = false;
+                }
+                else
+                {
+                    rectangleCount++;
+                    rectangles.Add(rectangle);
+
+                }
+                Refresh();
+            }
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            int rectCount = 0;
+            foreach (Rectangle rectangle in rectangles)
+            {
+                rectCount++;
+                e.Graphics.DrawRectangle(Pens.Red, rectangle);
+                e.Graphics.DrawString(rectCount.ToString(), Font, Brushes.Blue, rectangle.Location);
+            }
+
+            if (checkDraw)
+            {
+                Rectangle currentRectangle = new Rectangle(startPoint.X, startPoint.Y, endPoint.X - startPoint.X, endPoint.Y - startPoint.Y);
+                e.Graphics.DrawRectangle(Pens.Red, currentRectangle);
+                e.Graphics.DrawString(rectangleCount.ToString(), Font, Brushes.Blue, currentRectangle.Location);
+            }
+        }
+
+
+        private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Rectangle clickedRectangle = FindClickedRectangle(e.Location);
+                if (clickedRectangle != Rectangle.Empty)
+                {
+                    rectangles.Remove(clickedRectangle);
+                    Refresh();
+                }
+            }
+        }
+
+        private Rectangle FindClickedRectangle(Point clickLocation)
+        {
+            foreach (Rectangle rectangle in rectangles)
+            {
+                if (rectangle.Contains(clickLocation))
+                {
+                    return rectangle;
+                }
+            }
+            return Rectangle.Empty;
+        }
+        //Task 2
+        private void label1_MouseHover(object sender, EventArgs e)
+        {
+
+            label1.Location = new Point(Random.Shared.Next(1000), Random.Shared.Next(1000));
+
+        }
+
+        // Task 3
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            BackgroundImage = Resources.Baku;
+            DateTime dateTime = DateTime.Now;
+            MessageBox.Show(dateTime.ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            BackgroundImage = Resources.London;
+            DateTime dateTime = DateTime.UtcNow;
+            MessageBox.Show(dateTime.ToString());
+        }
+
+    }
+
+
+}
